14a98a35502640f7eaaef133809870777296d1eb
Kymaeda <kyo.m.720@gmail.com>
2021-11-23 11:03:43 +0900

    make App.js thinner

---
+++ screens/HomeScreen.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, FlatList } from 'react-native';
+import ListItem from '../components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const url = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  thumbnail: {
+    width: 100,
+    height: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  description: {
+    fontSize: 16,
+  },
+  source: {
+    fontSize: 12,
+    color: 'gray',
+  },
+});
+
+export default HomeScreen = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(url);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <ListItem
+      imageUrl={item.urlToImage}
+      description={item.title}
+      auther={item.auther}
+    />
+  );
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={renderItem}
+        keyExtractor={(_item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+};
